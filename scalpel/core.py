1df83ce1271bf8e3f678f38b3ae6452da1e9ee2b
v1ll4n <188228725@qq.com>
2016-12-20 16:23:31 +0800

    README.md added

---
+++ scalpel/core.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#coding:utf-8
+"""
+  Author:   --<v1ll4n>
+  Purpose: Render The Template
+  Created: 2016/12/20
+"""
+
+import unittest
+
+
+
+if __name__ == '__main__':
+    unittest.main()
