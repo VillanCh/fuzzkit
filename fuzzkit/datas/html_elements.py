f168fdc982432800b7453db1ad13221bc73a2b56
v1ll4n <188228725@qq.com>
2017-04-02 16:29:03 +0800

    add datas for common xss tags

---
+++ fuzzkit/datas/html_elements.py
@@ -0,0 +1,608 @@
+A = [
+     "download",
+     "href",
+     "hreflang",
+     "referrerpolicy",
+     "rel",
+     "target",
+     "type",
+     "charset",
+     "coords",
+     "name",
+     "rev",
+     "shape",
+]
+APPLET = [
+     "align",
+     "alt",
+     "archive",
+     "code",
+     "codebase",
+     "datafld",
+     "datasrc",
+     "height",
+     "hspace",
+     "mayscript",
+     "name",
+     "object",
+     "src",
+     "vspace",
+     "width",
+]
+AREA = [
+     "accesskey",
+     "alt",
+     "coords",
+     "download",
+     "href",
+     "hreflang",
+     "name",
+     "media",
+     "nohref",
+     "referrerpolicy",
+     "rel",
+     "shape",
+     "tabindex",
+     "target",
+     "type",
+]
+AUDIO = [
+     "autoplay",
+     "autobuffer",
+     "buffered",
+     "controls",
+     "loop",
+     "mozCurrentSampleOffset",
+     "muted",
+     "played",
+     "preload",
+     "src",
+     "volume",
+]
+BASE = [
+     "href",
+     "target",
+]
+BASEFONT = [
+     "color",
+     "face",
+     "size",
+]
+BGSOUND = [
+     "balance",
+     "loop",
+     "src",
+     "volume",
+]
+BLOCKQUOTE = [
+     "cite",
+]
+BODY = [
+     "alink",
+     "background",
+     "bgcolor",
+     "bottommargin",
+     "leftmargin",
+     "link",
+     "rightmargin",
+     "text",
+     "topmargin",
+     "vlink",
+]
+BR = [
+     "clear",
+]
+BUTTON = [
+     "autofocus",
+     "autocomplete",
+     "disabled",
+     "form",
+     "formaction",
+     "formenctype",
+     "formmethod",
+     "formnovalidate",
+     "formtarget",
+     "name",
+     "type",
+     "value",
+]
+CANVAS = [
+     "height",
+     "moz-opaque",
+     "width",
+]
+CAPTION = [
+     "align",
+]
+COL = [
+     "align",
+]
+COLGROUP = [
+     "align",
+     "bgcolor",
+     "char",
+     "charoff",
+     "span",
+     "valign",
+     "width",
+]
+COMMAND = [
+     "checked",
+     "disabled",
+     "icon",
+     "label",
+     "radiogroup",
+     "type",
+]
+CONTENT = [
+     "select",
+]
+DATA = [
+     "value",
+]
+DD = [
+     "nowrap",
+]
+DEL = [
+     "cite",
+     "datetime",
+]
+DETAILS = [
+     "open",
+]
+DIALOG = [
+     "open",
+]
+DIR = [
+     "compact",
+]
+DIV = [
+     "align",
+]
+DL = [
+     "compact",
+]
+EMBED = [
+     "height",
+     "src",
+     "type",
+     "width",
+]
+FIELDSET = [
+     "disabled",
+     "form",
+     "name",
+]
+FONT = [
+     "color",
+     "face",
+     "size",
+]
+FORM = [
+     "accept",
+     "accept-charset",
+     "action",
+     "autocapitalize",
+     "autocomplete",
+     "enctype",
+     "method",
+     "name",
+     "novalidate",
+     "target",
+]
+FRAME = [
+     "src",
+     "name",
+     "noresize",
+     "scrolling",
+     "marginheight",
+     "marginwidth",
+     "frameborder",
+]
+FRAMESET = [
+     "cols",
+     "rows",
+]
+HEAD = [
+     "profile",
+]
+HR = [
+     "align",
+     "color",
+     "noshade",
+     "size",
+     "width",
+]
+HTML = [
+     "manifest",
+     "version",
+     "xmlns",
+]
+IFRAME = [
+     "align",
+     "allowfullscreen",
+     "frameborder",
+     "height",
+     "longdesc",
+     "marginheight",
+     "marginwidth",
+     "mozallowfullscreen",
+     "webkitallowfullscreen",
+     "mozapp",
+     "mozbrowser",
+     "name",
+     "referrerpolicy",
+     "remote",
+     "scrolling",
+     "sandbox",
+     "src",
+     "srcdoc",
+     "width",
+]
+IMG = [
+     "align",
+     "alt",
+     "border",
+     "crossorigin",
+     "height",
+     "hspace",
+     "ismap",
+     "longdesc",
+     "name",
+     "referrerpolicy",
+     "sizes",
+     "src",
+     "srcset",
+     "width",
+     "usemap",
+     "vspace",
+]
+INPUT = [
+     "type",
+     "accept",
+     "accesskey",
+     "mozactionhint",
+     "autocapitalize",
+     "autocomplete",
+     "autocorrect",
+     "autofocus",
+     "capture",
+     "checked",
+     "disabled",
+     "form",
+     "formaction",
+     "formenctype",
+     "formmethod",
+     "formnovalidate",
+     "formtarget",
+     "height",
+     "incremental",
+     "inputmode",
+     "list",
+     "max",
+     "maxlength",
+     "min",
+     "minlength",
+     "multiple",
+     "multiple",
+     "name",
+     "pattern",
+     "placeholder",
+     "readonly",
+     "required",
+     "results",
+     "selectionDirection",
+     "selectionEnd",
+     "selectionStart",
+     "size",
+     "spellcheck",
+     "src",
+     "step",
+     "tabindex",
+     "usemap",
+     "value",
+     "webkitdirectory",
+     "width",
+     "x-moz-errormessage",
+]
+INS = [
+     "cite",
+     "datetime",
+]
+ISINDEX = [
+     "prompt",
+     "action",
+]
+KEYGEN = [
+     "autofocus",
+     "challenge",
+     "disabled",
+     "form",
+     "keytype",
+     "name",
+]
+LABEL = [
+     "for",
+     "form",
+]
+LI = [
+     "value",
+     "type",
+]
+LINK = [
+     "charset",
+     "crossorigin",
+     "disabled",
+     "href",
+     "hreflang",
+     "integrity",
+     "media",
+     "methods",
+     "referrerpolicy",
+     "rel",
+     "rev",
+     "sizes",
+     "target",
+     "title",
+     "type",
+]
+MAP = [
+     "name",
+]
+MARQUEE = [
+     "behavior",
+     "bgcolor",
+     "direction",
+     "height",
+     "hspace",
+     "loop",
+     "scrollamount",
+     "scrolldelay",
+     "truespeed",
+     "vspace",
+     "width",
+]
+MENU = [
+     "label",
+     "type",
+]
+MENUITEM = [
+     "checked",
+     "command",
+     "default",
+     "disabled",
+     "icon",
+     "label",
+     "radiogroup",
+     "type",
+]
+META = [
+     "charset",
+     "content",
+     "http-equiv",
+     "name",
+     "scheme",
+]
+METER = [
+     "value",
+     "min",
+     "max",
+     "low",
+     "high",
+     "optimum",
+     "form",
+]
+NEXTID = [
+     "n",
+]
+OBJECT = [
+     "archive",
+     "border",
+     "classid",
+     "codebase",
+     "codetype",
+     "data",
+     "declare",
+     "form",
+     "height",
+     "name",
+     "standby",
+     "tabindex",
+     "type",
+     "typemustmatch",
+     "usemap",
+     "width",
+]
+OL = [
+     "compact",
+     "reversed",
+     "start",
+     "type",
+]
+OPTGROUP = [
+     "disabled",
+     "label",
+]
+OPTION = [
+     "disabled",
+     "label",
+     "selected",
+     "value",
+]
+OUTPUT = [
+     "for",
+     "form",
+     "name",
+]
+PARAM = [
+     "name",
+     "type",
+     "value",
+     "valuetype",
+]
+PRE = [
+     "cols",
+     "width",
+     "wrap",
+]
+PROGRESS = [
+     "max",
+     "value",
+]
+Q = [
+     "cite",
+]
+SCRIPT = [
+     "async",
+     "integrity",
+     "src",
+     "type",
+     "text",
+     "language",
+     "defer",
+     "crossorigin",
+]
+SELECT = [
+     "autofocus",
+     "disabled",
+     "form",
+     "multiple",
+     "name",
+     "required",
+     "size",
+]
+SOURCE = [
+     "sizes",
+     "src",
+     "srcset",
+     "type",
+     "media",
+]
+SPACER = [
+     "type",
+     "size",
+     "width",
+     "height",
+     "align",
+]
+STYLE = [
+     "type",
+     "media",
+     "scoped",
+     "title",
+]
+TABLE = [
+     "align",
+     "bgcolor",
+     "border",
+     "cellpadding",
+     "cellspacing",
+     "frame",
+     "rules",
+     "summary",
+     "width",
+]
+TBODY = [
+     "align",
+     "bgcolor",
+     "char",
+     "charoff",
+     "valign",
+]
+TD = [
+     "abbr",
+     "align",
+     "axis",
+     "bgcolor",
+     "char",
+     "charoff",
+     "colspan",
+     "headers",
+     "rowspan",
+     "scope",
+     "valign",
+     "width",
+]
+TEXTAREA = [
+     "autocapitalize",
+     "autocomplete",
+     "autofocus",
+     "cols",
+     "disabled",
+     "form",
+     "maxlength",
+     "minlength",
+     "name",
+     "placeholder",
+     "readonly",
+     "required",
+     "rows",
+     "selectionDirection",
+     "selectionEnd",
+     "selectionStart",
+     "spellcheck",
+     "wrap",
+]
+TFOOT = [
+     "align",
+     "bgcolor",
+     "char",
+     "charoff",
+     "valign",
+]
+TH = [
+     "abbr",
+     "align",
+     "axis",
+     "bgcolor",
+     "char",
+     "charoff",
+     "colspan",
+     "headers",
+     "rowspan",
+     "scope",
+     "valign",
+     "width",
+]
+THEAD = [
+     "align",
+     "bgcolor",
+     "char",
+     "charoff",
+     "valign",
+]
+TIME = [
+     "datetime",
+]
+TR = [
+     "align",
+     "bgcolor",
+     "char",
+     "charoff",
+     "valign",
+]
+TRACK = [
+     "default",
+     "kind",
+     "label",
+     "src",
+     "srclang",
+]
+UL = [
+     "compact",
+     "type",
+]
+VIDEO = [
+     "autoplay",
+     "buffered",
+     "controls",
+     "crossorigin",
+     "height",
+     "loop",
+     "muted",
+     "played",
+     "preload",
+     "poster",
+     "src",
+     "width",
+]
---
+++ fuzzkit/datas/utils.py
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------------
+def print_list_from_file(filename, prefix=None):
+    """"""
+    _ret = open(filename)
+    if prefix:
+        print prefix + ' = ['
+    else:
+        print '['
+    for i in _ret:
+        text = i.strip()
+        print '    "{}",'.format(text)
+    print ']'
+    
+    
+import os
+from utils import print_list_from_file
+
+base = 'data/svg_tag_attributes/'
+
+file_list = None
+for i in os.walk(base):
+    file_list = map(lambda x: base + x, i[2])
+
+for i in file_list:
+    _i = os.path.basename(i).replace('-', '_')
+    _i = _i.split('.txt')[0].upper()
+    print_list_from_file(i, _i)
+
+
