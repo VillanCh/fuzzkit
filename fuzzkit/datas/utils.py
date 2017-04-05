ae987dfc94768c79f03a048da9cbeb5053128c82
v1ll4n <188228725@qq.com>
2017-04-05 15:04:46 +0800

    HTML tag data

--- fuzzkit/datas/utils.py
+++ fuzzkit/datas/utils.py
@@ -12,18 +12,18 @@ def print_list_from_file(filename, prefix=None):
     print ']'
     
     
-import os
-from utils import print_list_from_file
+#import os
+#from utils import print_list_from_file
 
-base = 'data/svg_tag_attributes/'
+#base = 'data/svg_tag_attributes/'
 
-file_list = None
-for i in os.walk(base):
-    file_list = map(lambda x: base + x, i[2])
+#file_list = None
+#for i in os.walk(base):
+    #file_list = map(lambda x: base + x, i[2])
 
-for i in file_list:
-    _i = os.path.basename(i).replace('-', '_')
-    _i = _i.split('.txt')[0].upper()
-    print_list_from_file(i, _i)
+#for i in file_list:
+    #_i = os.path.basename(i).replace('-', '_')
+    #_i = _i.split('.txt')[0].upper()
+    #print_list_from_file(i, _i)
 
 
